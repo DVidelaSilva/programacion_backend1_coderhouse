7f17136561e57924a6a3587b3bb457654e7652a5
DAVS <41497687+DVidelaSilva@users.noreply.github.com>
2024-06-25 19:35:03 -0400

    Clase 0

---
+++ 01.clase/01.App.js
@@ -0,0 +1,36 @@
+/* console.log('Hola DAVS');
+
+console.warn('Alerta');
+
+console.error('Error'); */
+
+
+
+//  Tipos de Datos
+
+// Tipos Primitivos
+
+//let  -> Puede cambiar
+//const  -> No Puede cambiar
+//var   -> Puede cambiar y es global, YA NO SE USA
+
+
+let nombre = 'Diego';
+console.log(nombre);
+console.log('---------------------');
+nombre = 'Arturo'
+console.log(nombre);
+
+
+let array = ['Diego', 'Lucky', 'Nicole']
+console.log(array);
+
+
+
+let edad = 44;
+
+console.log(edad);
+
+edad = edad + 1;
+
+console.log(edad);
